3a5deb6d5ceb71d40557543103ab28cc1c0ed8b8
Ankit Kumar Yadav <67411716+AnkitKrYadav@users.noreply.github.com>
2025-09-17 08:17:09 +0530

    Add cfround1001_1.py for competitive programming tasks
    This script includes functions for inputting integers, lists, and matrices, and reversing numbers.

---
+++ Competitive Programming/cfround1001_1.py
@@ -0,0 +1,21 @@
+import math,collections
+#int input
+'''n = int(input("Enter n: "))'''
+
+#list_str_input
+'''lsi = input("Enter space separated row: ").strip().split()'''
+
+#list_int_input
+'''lii = list(map(int,input("Enter space separated int row: ").strip().split()))'''
+
+# matrix input nxm
+'''
+n = int(input("no. of rows: "))
+m = int(input("no. of columns: "))
+mat = []
+for i in range (n):
+    lii = list(map(int,input("Enter space separated row no. {i}: ").strip().split()))
+    matrix.append(lii)
+'''
+#reverse num
+'''rev_num = int(str(num)[::-1])'''
