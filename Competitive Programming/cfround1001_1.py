d84caee1a49a6a455dac7792b2629cba6199667e
Ankit Kumar Yadav <67411716+AnkitKrYadav@users.noreply.github.com>
2025-09-19 17:43:52 +0000

    updated

--- Competitive Programming/cfround1001_1.py
+++ Competitive Programming/cfround1001_1.py
@@ -1,21 +1,28 @@
-import math,collections
-#int input
-'''n = int(input("Enter n: "))'''
+import math as m
+imort sys
+''' n = int(input("Enter n: ")) '''
 
-#list_str_input
-'''lsi = input("Enter space separated row: ").strip().split()'''
+''' lsi = input("Enter space separated row: ").strip().split() '''
 
-#list_int_input
-'''lii = list(map(int,input("Enter space separated int row: ").strip().split()))'''
+''' lii = list(map(int,input("Enter space separated int row: ").strip().split())) '''
 
-# matrix input nxm
 '''
 n = int(input("no. of rows: "))
 m = int(input("no. of columns: "))
 mat = []
-for i in range (n):
-    lii = list(map(int,input("Enter space separated row no. {i}: ").strip().split()))
-    matrix.append(lii)
+for i in range (1,n+1):
+    lii = list(map(int,input(f"Enter space separated row no. {i}: ").strip().split()))
+    mat.append(lii)
 '''
-#reverse num
-'''rev_num = int(str(num)[::-1])'''
+
+''' rev_num = int(str(num)[::-1]) '''
+
+'''
+def gcd(a,b):
+    while b:
+        a,b = b,a%b
+    return a
+
+    def lcm(a,b):
+        return (a*b)//gcd(a,b)
+'''
